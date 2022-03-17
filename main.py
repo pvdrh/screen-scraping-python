49d9819f77df174fbeeb333a5dac18b6d406696f
Phạm Văn Duy <637915@sv.vnua.edu.vn>
2022-03-17 14:45:07 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+
+
+def find_jobs():
+    html_text = requests.get('https://topdev.vn/viec-lam-it/python-kt34').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('div', class_='box-job')
+    for index, job in enumerate(jobs):
+        company_name = job.find('p', class_='job-location fl mb-1').text.replace(' ', '')
+        position = job.find('h3').text.replace(' ', '')
+        more_info = job.h3.a['href']
+        with open(f'data/{index}.txt', 'w', encoding='utf-8') as f:
+            f.write(f"Company Name: {company_name.strip()}")
+            f.write(f"Position: {position.strip()}")
+            f.write(f"More Info: https://topdev.vn{more_info}")
+        print(f'File save: {index}')
+
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Watting {time_wait} seconds...')
+        time.sleep(time_wait * 1)
